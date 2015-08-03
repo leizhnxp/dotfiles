d6b85e77a3c7821f1f0be2dc9d592b560ea7b615
leizhnxp <leizhnxp@gmail.com>
2015-08-03 09:35:00 +0800

    make url of oss zip parameter

--- tools/install_oss.sh
+++ tools/install_oss.sh
@@ -4,7 +4,7 @@ cd
 mkdir -p ~/oss/
 mkdir -p ~/bin/
 cd ~/oss/
-wget "https://docs-aliyun.cn-hangzhou.oss.aliyun-inc.com/cn/oss/0.2.7/assets/sdk/OSS_Python_API_20150707.zip?spm=5176.730001.3.74.2uMOcb&Expires=1438524064&OSSAccessKeyId=80kJOHQaA4syuazx&Signature=DU6LOrSAsqjovLJPu9gwM1zCHHk%3D" -O OSS_Python_API_20150707.zip
-unzip OSS_Python_API_20150707.zip
+wget $1 -O OSS_Python_API.zip
+unzip OSS_Python_API.zip
 ln -s ~/oss/osscmd ~/bin/osscmd
 
