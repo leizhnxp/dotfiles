c68c4c7b09f430a85cd3613ff1cef7afd3b7f8c8
leizhnxp <leizhnxp@gmail.com>
2015-08-13 17:20:16 +0800

    modify centos6 repo install scripts

--- tools/addition_cent6_base_repo.sh
+++ tools/addition_cent6_base_repo.sh
@@ -2,7 +2,7 @@
 
 self_dir=$(readlink -m $0|xargs dirname)
 
-sudo cp $self_dir/etc/CentOS6-Base-163.repo /etc/yum.repos.d/
+sudo cp $self_dir/etc/CentOS6-Base-rework.repo /etc/yum.repos.d/
 
 sudo yum clean all
 sudo yum makecache
