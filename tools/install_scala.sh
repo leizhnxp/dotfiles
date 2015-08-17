72016f5fa9b5702044eb625b98e8346a4be1f4aa
leizhnxp <leizhnxp@gmail.com>
2015-08-17 15:58:17 +0800

    change scala install scripts

--- tools/install_scala.sh
+++ tools/install_scala.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-#
-# http://decisionstats.com/2014/04/15/installing-scala-on-centos/
-#
 cd /opt
 sudo wget "http://downloads.typesafe.com/scala/2.11.7/scala-2.11.7.tgz"
-sudo tar xvf scala-2.10.1.tgz
-sudo ln -sf /opt/scala-2.10.1 /opt/scala
+sudo tar xvf scala-2.11.7.tgz
+sudo ln -sf /opt/scala-2.11.7 /opt/scala
 scala -version
