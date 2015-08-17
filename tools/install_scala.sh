3a33c8716169a6a04d2509954d7c47715eda6ef9
leizhnxp <leizhnxp@gmail.com>
2015-08-17 16:00:48 +0800

    change scala install scripts

--- tools/install_scala.sh
+++ tools/install_scala.sh
@@ -4,4 +4,5 @@ cd /opt
 sudo wget "http://downloads.typesafe.com/scala/2.11.7/scala-2.11.7.tgz"
 sudo tar xvf scala-2.11.7.tgz
 sudo ln -sf /opt/scala-2.11.7 /opt/scala
+sudo rm -rf scala-2.11.7.tgz
 scala -version
