c726ac6040192f02d96a7bc1cfa5d35d417a2951
leizhnxp <leizhnxp@gmail.com>
2015-08-21 15:50:56 +0800

    add a lovely tools

--- tools/install_components.sh
+++ tools/install_components.sh
@@ -2,5 +2,5 @@
 
 # sudo yum update -y
 sudo yum install epel-release ctags vim git wget curl tops-osscmd -y
-sudo yum install nodejs nginx npm screen nc openconnect nmap gdal -y
+sudo yum install nodejs nginx npm screen nc openconnect nmap gdal tree -y
 
