bb426c0408b842c5f0d5be153a71920c4da4c9fc
leizhnxp <leizhnxp@gmail.com>
2015-08-27 16:43:35 +0800

    add graphviz

--- tools/install_components.sh
+++ tools/install_components.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # sudo yum update -y
-sudo yum install epel-release ctags vim git wget curl tops-osscmd -y
+sudo yum install epel-release ctags vim git wget curl tops-osscmd graphviz -y
 sudo yum install nodejs nginx npm screen nc openconnect nmap gdal tree -y
 
