91336576df3d67a25a7f60945d897b7f976fdf41
leizhnxp <leizhnxp@gmail.com>
2015-09-11 16:21:47 +0000

    modify for os x

--- install.sh
+++ install.sh
@@ -21,9 +21,9 @@ refresh_dotfile gitignore
 refresh_dotfile screenrc
 refresh_dotfile dir_colors
 refresh_dotfile bashrc
-refresh_dotfile bash_profile
 
 [[ $osname == "Darwin" ]] && exit 0
 
 refresh_dotfile git-completion.bash
+refresh_dotfile bash_profile
 
