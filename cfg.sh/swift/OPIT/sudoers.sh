28000d8f87d44c0cd68386bd9aedca7acf7fd2ad
leizhnxp <leizhnxp@gmail.com>
2016-03-16 06:58:49 +0000

    add shebang

--- cfg.sh/swift/OPIT/sudoers.sh
+++ cfg.sh/swift/OPIT/sudoers.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+
 sudo cp /home/zhenhua.lei/dotfiles/cfg.etc/sudoers.d/leizhenhua /etc/sudoers.d/
 sudo cp /home/zhenhua.lei/dotfiles/cfg.etc/rsyslog.d/sudo.conf /etc/rsyslog.d/
