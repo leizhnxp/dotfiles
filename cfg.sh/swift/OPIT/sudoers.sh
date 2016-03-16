1754d3ee34258979fd65c9da8b9cc7e1d67eb511
leizhnxp <leizhnxp@gmail.com>
2016-03-16 07:02:29 +0000

    add restart rsyslogd

--- cfg.sh/swift/OPIT/sudoers.sh
+++ cfg.sh/swift/OPIT/sudoers.sh
@@ -2,3 +2,4 @@
 
 sudo cp /home/zhenhua.lei/dotfiles/cfg.etc/sudoers.d/leizhenhua /etc/sudoers.d/
 sudo cp /home/zhenhua.lei/dotfiles/cfg.etc/rsyslog.d/sudo.conf /etc/rsyslog.d/
+sudo service rsyslog restart
