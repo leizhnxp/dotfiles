e9815cd57080492adec2200ae20995ce056ac50e
leizhnxp <leizhnxp@gmail.com>
2016-03-19 08:41:28 +0000

    fixed other account error

--- cfg.sh/swift/OPIT/sudoers.sh
+++ cfg.sh/swift/OPIT/sudoers.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-sudo cp /home/zhenhua.lei/dotfiles/cfg.etc/sudoers.d/leizhenhua /etc/sudoers.d/
-sudo cp /home/zhenhua.lei/dotfiles/cfg.etc/rsyslog.d/sudo.conf /etc/rsyslog.d/
+sudo cp ~/dotfiles/cfg.etc/sudoers.d/leizhenhua /etc/sudoers.d/
+sudo cp ~/dotfiles/cfg.etc/rsyslog.d/sudo.conf /etc/rsyslog.d/
+
 sudo service rsyslog restart
