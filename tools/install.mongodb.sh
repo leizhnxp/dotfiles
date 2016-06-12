de71e2f429b3c810a5e0c84b68f4cc1edcf69eae
leizhnxp <leizhnxp@gmail.com>
2016-06-12 03:19:54 +0000

    add mongodb repo and install

---
+++ tools/install.mongodb.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash    
+sudo yum install wget vim -y
+cd /etc/yum.repos.d/
+sudo wget https://repo.mongodb.org/yum/redhat/mongodb-org.repo
+sudo yum install mongodb-org -y
+sudo service mongod restart
