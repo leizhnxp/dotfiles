53578698bdfa547f1d258c1e9a8e6320914c228d
zhenhua.lei <leizhenhua@nationsky.com>
2016-06-24 07:23:54 +0000

    new file:   tools/install_nodejs_v4_LTS_yum_repo.sh

---
+++ tools/install_nodejs_v4_LTS_yum_repo.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+### view https://nodejs.org/en/download/package-manager/#enterprise-linux-and-fedora
+
+sudo curl --silent --location https://rpm.nodesource.com/setup_4.x | sudo bash -
