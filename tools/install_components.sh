6b551538a13432e3f5aae6522ad723c3a990f6b2
zhenhua.lei <leizhenhua@nationsky.com>
2016-07-04 09:15:11 +0000

    add 7z support

--- tools/install_components.sh
+++ tools/install_components.sh
@@ -2,5 +2,5 @@
 
 # sudo yum update -y
 sudo yum install epel-release ctags vim git wget curl tops-osscmd graphviz -y
-sudo yum install nodejs nginx npm screen nc openconnect nmap gdal tree -y
+sudo yum install nodejs nginx npm screen nc openconnect nmap gdal tree p7zip -y
 
