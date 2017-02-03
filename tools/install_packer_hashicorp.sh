01a037d01a8207f5bce12726a8bd1efe32e9ab80
leizh <leizhnxp@gmail.com>
2017-02-03 16:33:25 +0800

    version to variable

--- tools/install_packer_hashicorp.sh
+++ tools/install_packer_hashicorp.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-wget https://releases.hashicorp.com/packer/0.12.1/packer_0.12.1_linux_amd64.zip -O /tmp/packer.zip
+ver=0.12.2
+wget https://releases.hashicorp.com/packer/$ver/packer_${ver}_linux_amd64.zip -O /tmp/packer.zip
 
 mkdir -p ~/bin
+rm -rf ~/bin/packer
 unzip /tmp/packer.zip -d ~/bin/
+rm -rf /tmp/packer.zip
 
