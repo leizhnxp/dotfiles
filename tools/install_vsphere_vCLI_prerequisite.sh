0c28d705ce5209f13a637ad50d4d19d391510c7d
zhenhua.lei <leizhenhua@nationsky.com>
2017-02-07 18:39:10 +0800

    vCLI required

---
+++ tools/install_vsphere_vCLI_prerequisite.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+yum install e2fsprogs-devel libuuid-devel openssl-devel perl-devel
+yum install glibc.i686 zlib.i686
+yum install perl-XML-LibXML libncurses.so.5 perl-Crypt-SSLeay
+
