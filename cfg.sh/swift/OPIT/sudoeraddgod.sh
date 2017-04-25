05d0285bff9ba6237fdd9ae178f7684c439cd119
zhenhua.lei <leizhenhua@nationsky.com>
2017-04-25 11:20:17 +0800

    管理sudoers配置

--- cfg.sh/swift/OPIT/sudoeraddgod.sh
+++ cfg.sh/swift/OPIT/sudoeraddgod.sh
@@ -5,3 +5,5 @@ filename=${1/\./""}
 echo $username $filename 
 sudo useradd $username
 echo "$username ALL=(ALL) ALL" | sudo tee /etc/sudoers.d/$filename
+echo "Defaults:$username  timestamp_timeout=1000" | sudo tee -a /etc/sudoers.d/$filename
+
