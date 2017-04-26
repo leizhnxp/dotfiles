9c20887e51c75b3e7c559e9aef361e3b97cb99d6
zhenhua.lei <leizhnxp@gmail.com>
2017-04-26 23:12:37 +0800

    bashrc independency

--- install.sh
+++ install.sh
@@ -8,20 +8,23 @@ mkdir -p ~/opt
 
 refresh_dotfile(){
     echo handle symlink $1
-    [ -f ~/.$1 ] && mv ~/.$1 ~/.backup/dotfiles/$1.bk.$ts
+    backup_dotfile $1
     ln -sf ~/dotfiles/files/$1 ~/.$1
+}
+
+backup_dotfile(){
+    [ -f ~/.$1 ] && cp ~/.$1 ~/.backup/dotfiles/$1.bk.$ts
 }
 
 [[ $osname == "Linux" ]] && uname -a
 
 [ -d ~/.vim/bundle/Vundle.vim ] && echo need not to clone || git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-
 refresh_dotfile vimrc
 refresh_dotfile gitignore
 refresh_dotfile screenrc
 refresh_dotfile dir_colors
-refresh_dotfile bashrc
+# refresh_dotfile bashrc
 refresh_dotfile git-completion.bash
 
 git config --global color.ui true
@@ -36,4 +39,6 @@ if [ $osname == "Darwin" ]; then
     refresh_dotfile bash_profile
 fi
 
+backup_dotfile bashrc
+grep "bashrc.my.sh" ~/.bashrc || echo $'\n''source ~/dotfiles/files/bashrc.my.sh'$'\n' >> ~/.bashrc
 
