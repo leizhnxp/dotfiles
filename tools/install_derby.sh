fc2ecb4106b4873dac79b3ea686bba528360a37a
zhenhua.lei <leizhnxp@gmail.com>
2017-07-12 17:48:59 +0800

    增加一个derby安装，主要是为了ij

---
+++ tools/install_derby.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/opt/
+cd ~/opt/
+
+
+wget https://mirrors.tuna.tsinghua.edu.cn/apache/db/derby/db-derby-10.13.1.1/db-derby-10.13.1.1-bin.tar.gz
+tar xvf db-derby-10.13.1.1-bin.tar.gz
+#export DERBY_INSTALL=~/opt/db-derby-10.13.1.1-bin
+
