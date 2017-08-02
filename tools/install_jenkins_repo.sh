31107372c051265a6c0d5d6820f060e12fe5f9cd
zhenhua.lei <leizhenhua@nationsky.com>
2017-08-02 11:46:44 +0800

    like file name ...

---
+++ tools/install_jenkins_repo.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+
