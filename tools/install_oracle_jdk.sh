4fead14b8b4342803bca0e3554f52fd9ec310b31
zhenhua.lei <leizhenhua@nationsky.com>
2017-08-02 13:42:30 +0800

    oracle这个变态...

--- tools/install_oracle_jdk.sh
+++ tools/install_oracle_jdk.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-JAVA_VERSION=8u73
-BUILD_VERSION=b02
-wget --no-cookies --no-check-certificate --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/$JAVA_VERSION-$BUILD_VERSION/jdk-$JAVA_VERSION-linux-x64.rpm" -O /tmp/jdk-$JAVA_VERSION-$BUILD_VERSION-linux-x64.rpm
+JAVA_VERSION=8u144
+BUILD_VERSION=b01
+wget --no-cookies --no-check-certificate --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u144-b01/090f390dda5b47b9b721c7dfaa008135/jdk-8u144-linux-x64.rpm" -O /tmp/jdk-$JAVA_VERSION-$BUILD_VERSION-linux-x64.rpm
 sudo rpm -ivh /tmp/jdk-$JAVA_VERSION-$BUILD_VERSION-linux-x64.rpm
 rm -f /tmp/jdk-$JAVA_VERSION-$BUILD_VERSION-linux-x64.rpm
