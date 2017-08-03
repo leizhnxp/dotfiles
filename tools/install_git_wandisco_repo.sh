98fedbc4616800c8105877e88b5db2fccb2b0720
zhenhua.lei <leizhenhua@nationsky.com>
2017-08-03 16:04:29 +0800

    专治各种scm工具最新版？

---
+++ tools/install_git_wandisco_repo.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sudo yum install wget -y 
+
+cd /tmp/
+wget http://opensource.wandisco.com/centos/7/git/x86_64/wandisco-git-release-7-1.noarch.rpm
+
+sudo rpm ivh wandisco-git-release-7-1.noarch.rpm
+rm -rf wandisco-git-release-7-1.noarch.rpm
+
