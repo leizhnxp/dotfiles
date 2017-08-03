2a42dbdf520d76ea0846a311dcb7123213ec8411
zhenhua.lei <leizhenhua@nationsky.com>
2017-08-03 16:07:38 +0800

    这也能写错...

--- tools/install_git_wandisco_repo.sh
+++ tools/install_git_wandisco_repo.sh
@@ -5,6 +5,6 @@ sudo yum install wget -y
 cd /tmp/
 wget http://opensource.wandisco.com/centos/7/git/x86_64/wandisco-git-release-7-1.noarch.rpm
 
-sudo rpm ivh wandisco-git-release-7-1.noarch.rpm
+sudo rpm -ivh wandisco-git-release-7-1.noarch.rpm
 rm -rf wandisco-git-release-7-1.noarch.rpm
 
