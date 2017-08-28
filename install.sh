a50a375af0e1f0ccad7ee004ca9a525c67c12d06
zhenhua.lei <leizhenhua@nationsky.com>
2017-08-28 16:52:57 +0800

    etc的不好啊，可是为了不多写几行source我竟然忍了

--- install.sh
+++ install.sh
@@ -19,8 +19,8 @@ backup_dotfile(){
 [[ $osname == "Linux" ]] && uname -a
 
 wget https://raw.githubusercontent.com/juven/maven-bash-completion/master/bash_completion.bash -O ~/.maven_bash_completion.bash
-curl -L https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/bash/docker -o /etc/bash_completion.d/docker
-curl -L https://raw.githubusercontent.com/docker/compose/master/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+sudo curl -L https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/bash/docker -o /etc/bash_completion.d/docker
+sudo curl -L https://raw.githubusercontent.com/docker/compose/master/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
 
 [ -d ~/.vim/bundle/Vundle.vim ] && echo need not to clone || git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
