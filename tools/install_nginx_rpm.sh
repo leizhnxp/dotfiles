8a209166ae7124c2fd901564ae4b0a2f80c04755
leizh <leizhenhua@arrail-dental.com>
2018-10-17 15:54:17 +0800

    drop the deprecated scripts

--- tools/install_nginx_rpm.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd /opt
-sudo wget http://nginx.org/packages/rhel/6/noarch/RPMS/nginx-release-rhel-6-0.el6.ngx.noarch.rpm
-sudo rpm -ivh nginx-release-rhel-6-0.el6.ngx.noarch.rpm
-sudo rm -rf nginx-release-rhel-6-0.el6.ngx.noarch.rpm
