cadcf363f8dcf8a17f71fce6fd39e2a592583a01
leizh <leizhenhua@arrail-dental.com>
2019-01-22 10:25:09 +0800

    modify PS1 and history size
    + handling PS1 with docker machine
    + make history size up to 10000

--- files/bashrc.my.sh
+++ files/bashrc.my.sh
@@ -10,6 +10,7 @@ alias wanip='dig +short myip.opendns.com @resolver1.opendns.com'
 
 HISTTIMEFORMAT="%Y%m%d-%H%M%S: "
 export HISTTIMEFORMAT
+export HISTSIZE=10000
 
 #export JAVA_HOME=/usr/java/default
 #export M2_HOME=~/opt/apache-maven-3.3.9
@@ -23,22 +24,20 @@ export GOPATH=~/gocode
 export PATH=$HOME/bin:$PATH:$PH_HOME/bin
 
 export EDITOR=vim
+export LANG=en_US.UTF-8
+export TERM=xterm-256color
+export PS1='[\[\033[01;32m\]\u@\H \[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]]\\$ '
 
-if [ -f ~/.git-completion.bash ]; then
-    source ~/.git-completion.bash
-fi
-
+source <(kubectl completion bash)
 [ -f ~/.maven_bash_completion.bash ] && source ~/.maven_bash_completion.bash
 [ -f ~/.completion_docker-machine.bash ] && source ~/.completion_docker-machine.bash
 [ -f ~/.completion_docker-machine-wrapper.bash ] && source ~/.completion_docker-machine-wrapper.bash
-[ -f ~/.completion_docker-machine-prompt.bash ] && source ~/.completion_docker-machine-prompt.bash
 [ -f ~/.docker-completion.sh ] && source ~/.docker-completion.sh
 [ -f ~/.dockr-compose-completion.sh ] && source ~/.docker-compose-completion.sh
+[ -f ~/.git-completion.bash ] && source ~/.git-completion.bash
 
-source <(kubectl completion bash)
-
-export LANG=en_US.UTF-8
-export TERM=xterm-256color
-export PS1='[\[\033[01;32m\]\u@\H \[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]$(__docker_machine_ps1 " [%s]")]\\$ '
-
+if [ -f ~/.completion_docker-machine-prompt.bash ]; then
+  source ~/.completion_docker-machine-prompt.bash
+  export PS1='[\[\033[01;32m\]\u@\H \[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]$(__docker_machine_ps1 " [%s]")]\\$ '
+fi
 
