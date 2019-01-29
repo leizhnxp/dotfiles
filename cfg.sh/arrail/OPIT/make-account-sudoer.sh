33a0586e8316064368f9116357a09817901d8dfa
leizh <leizhenhua@arrail-dental.com>
2019-01-29 15:11:03 +0800

    add ls to sudoer command list

--- cfg.sh/arrail/OPIT/make-account-sudoer.sh
+++ cfg.sh/arrail/OPIT/make-account-sudoer.sh
@@ -3,7 +3,8 @@
 username=$1
 filename=${1/\./""}
 echo $username $filename
-echo "$username ALL=(ALL) /usr/bin/whoami,/usr/bin/who" | sudo tee /etc/sudoers.d/$filename
+echo "$username ALL=(ALL) /usr/bin/whoami,/bin/ls /*" | sudo tee /etc/sudoers.d/$filename
 # never do like blow ...
 # echo "$username ALL=(ALL) ALL,!/usr/bin/vim /etc/sudoers,!/usr/bin/vi /etc/sudoers,!/sbin/visudo,!/usr/sbin/visudo,!/usr/bin/passwd,!/usr/bin/su,!/usr/bin/bash" | sudo tee /etc/sudoers.d/$filename
-echo "Defaults:$username  timestamp_timeout=20" | sudo tee -a /etc/sudoers.d/$filename
+echo "Defaults:$username  timestamp_timeout=10" | sudo tee -a /etc/sudoers.d/$filename
+
