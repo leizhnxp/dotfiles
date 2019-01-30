8043f5a7b8d6acf150f6c5b878e9a6d4b9f9ecf4
leizh <leizhenhua@arrail-dental.com>
2019-01-30 17:11:02 +0800

    3 log prepare

--- cfg.sh/history-audit-log-creation.sh
+++ cfg.sh/history-audit-log-creation.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 
-mkdir -p /var/log/history-audit/ && touch /var/log/history-audit/$(date +%Y-%m-%d).log
+mkdir -p /var/log/history-audit/
+
+FILE_TDY=/var/log/history-audit/$(date +%Y-%m-%d).log
+FILE_TMR=/var/log/history-audit/$(date +%Y-%m-%d -d tomorrow).log
+FILE_DTM=/var/log/history-audit/$(date +%Y-%m-%d -d "-2 days ago").log
+
+touch $FILE_TDY $FILE_TMR $FILE_DTM
+
+chown nobody:nobody $FILE_TDY $FILE_TMR $FILE_DTM
+
+chmod 002 $FILE_TDY $FILE_TMR $FILE_DTM
+chattr +a $FILE_TDY $FILE_TMR $FILE_DTM
+
 
