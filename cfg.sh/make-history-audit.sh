5cfc7eb79203d212f9a38d9e55ce4acfb0a59956
leizh <leizhenhua@arrail-dental.com>
2019-01-30 17:28:13 +0800

    system admin management script

---
+++ cfg.sh/make-history-audit.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#sudo curl -L https://raw.githubusercontent.com/leizhnxp/dotfiles/master/files/history-audit-profile.sh -o /etc/profile.d/system-audit.sh
+
+echo '* * * * * root curl https://raw.githubusercontent.com/leizhnxp/dotfiles/master/cfg.sh/history-audit-log-creation.sh | bash' | sudo tee /etc/cron.d/system-audit-log-creation
+
