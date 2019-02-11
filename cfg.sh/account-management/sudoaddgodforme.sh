df6aa694b64707f0026b00cdcf72404077579af1
leizh <leizhenhua@arrail-dental.com>
2019-02-11 15:35:38 +0800

    modify base script

--- cfg.sh/account-management/sudoaddgodforme.sh
+++ cfg.sh/account-management/sudoaddgodforme.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-curl https://raw.githubusercontent.com/leizhnxp/dotfiles/master/cfg.sh/arrail/OPIT/sudoeraddgod.sh | bash -s zhenhua.lei "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCfXvlBAIsE4/gQIvQT/I/3ZOptPK+gvhOMDd3pCFbcm4SfYXkUtSlmumuSWoP5XHBpSsBL9ZPYPpXzMrjMn0GcvV5PGu6sX7Ehv0mHB5I/AaLgrcbYcMR+T5JilssucXp/KJFJmT292m6f8fxWlVIFwxzO16RadMI48LGhW+iUFEoyvKz3f9Q0h5VzKRj/tg0qMq+AtatjT7ccd50iRONKsuY53JcFieRHZbbtuUVIcmjP/5phUzI8hAsURE3AxJgzVp092dwvtS7Jr56ArnAkPlLSfy/qTquigLrZXfTzWCuUEmBDowX1Gv/pm5+JK3F7x1pybFmy10RMZpqTeE/X zhenhua.lei@t460"
+https://raw.githubusercontent.com/leizhnxp/dotfiles/master/cfg.sh/account-management/add-god-account-with-public-keys.sh | bash -s zhenhua.lei "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCfXvlBAIsE4/gQIvQT/I/3ZOptPK+gvhOMDd3pCFbcm4SfYXkUtSlmumuSWoP5XHBpSsBL9ZPYPpXzMrjMn0GcvV5PGu6sX7Ehv0mHB5I/AaLgrcbYcMR+T5JilssucXp/KJFJmT292m6f8fxWlVIFwxzO16RadMI48LGhW+iUFEoyvKz3f9Q0h5VzKRj/tg0qMq+AtatjT7ccd50iRONKsuY53JcFieRHZbbtuUVIcmjP/5phUzI8hAsURE3AxJgzVp092dwvtS7Jr56ArnAkPlLSfy/qTquigLrZXfTzWCuUEmBDowX1Gv/pm5+JK3F7x1pybFmy10RMZpqTeE/X zhenhua.lei@t460"
 
