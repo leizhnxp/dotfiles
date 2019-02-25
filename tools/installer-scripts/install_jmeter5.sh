9849fb9e9521e63d446bc7f67150f4532a355c11
leizh <leizhenhua@arrail-dental.com>
2019-02-25 18:11:59 +0800

    change package name

--- tools/installer-scripts/install_jmeter5.sh
+++ tools/installer-scripts/install_jmeter5.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+set -e
+
 cd 
 mkdir -p tmp
 cd tmp
 wget http://mirrors.tuna.tsinghua.edu.cn/apache/jmeter/binaries/apache-jmeter-5.1.tgz
 
-tar xvf apache-jmeter-5.0.tgz -C ~/opt/
+tar xvf apache-jmeter-5.1.tgz -C ~/opt/
+rm apache-jmeter-5.1.tgz
 
-mkdir ~/bin
-ln -sf ~/bin/jmeter ~/opt/apache-jmeter-5.0/bin/jmeter
+mkdir -p ~/bin
+ln -sf ~/bin/jmeter ~/opt/apache-jmeter-5.1/bin/jmeter
 
