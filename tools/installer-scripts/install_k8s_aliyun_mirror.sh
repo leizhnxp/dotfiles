80a698bc777c804e09e7e0abdbdab3395001405f
leizh <leizhenhua@arrail-dental.com>
2019-02-27 11:09:14 +0800

    modify k8s aliyun mirror

--- tools/installer-scripts/install_k8s_aliyun_mirror.sh
+++ tools/installer-scripts/install_k8s_aliyun_mirror.sh
@@ -15,6 +15,6 @@ gpgcheck=1
 repo_gpgcheck=1
 gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
 EOF
-setenforce 0
+# setenforce 0
 # yum install -y kubelet kubeadm kubectl
 
