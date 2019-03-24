cda304d53d023310487b727214283e2c1709fc21
leizh <leizhenhua@arrail-dental.com>
2019-03-24 12:06:24 +0800

    add helm completion

--- files/bashrc.my.sh
+++ files/bashrc.my.sh
@@ -46,6 +46,7 @@ case $TERM in
 esac
 
 type kubectl 1>&2 && source <(kubectl completion bash)
+type helm 1>&2 && source <(helm completion bash)
 [ -f ~/.maven_bash_completion.bash ] && source ~/.maven_bash_completion.bash
 [ -f ~/.completion_docker-machine.bash ] && source ~/.completion_docker-machine.bash
 [ -f ~/.completion_docker-machine-wrapper.bash ] && source ~/.completion_docker-machine-wrapper.bash
