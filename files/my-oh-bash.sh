8476e17e8c24d0af0c5bd5bd6539a38528d28d07
雷振华 <leizhnxp@gmail.com>
2021-10-02 16:10:37 +0800

    compatible to modify
    + screen reset only in Linux

--- files/my-oh-bash.sh
+++ files/my-oh-bash.sh
@@ -2,6 +2,7 @@
 
 echo hello in bash?
 
-ls /var/run/screen/S-$USER | xargs -i -n1 echo screen -S {} -X setenv SSH_AUTH_SOCK $SSH_AUTH_SOCK
-ls /var/run/screen/S-$USER | xargs -i -n1 screen -S {} -X setenv SSH_AUTH_SOCK $SSH_AUTH_SOCK
+# only in native Linux or WSL
+# Cygwin or OSX skipped
+uname -a | grep Linux && ls /var/run/screen/S-$USER | xargs -i -n1 screen -S {} -X setenv SSH_AUTH_SOCK $SSH_AUTH_SOCK
 
