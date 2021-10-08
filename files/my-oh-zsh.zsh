a1f93bf0182e0f71782426bd676863d82d1e998b
leizh <leizhnxp@gmail.com>
2021-10-08 17:54:57 +0800

    enable pageant on WSL2
    + only in omz

--- files/my-oh-zsh.zsh
+++ files/my-oh-zsh.zsh
@@ -21,3 +21,19 @@ uname -a | grep Linux && ls /var/run/screen/S-$USER | xargs -i -n1 screen -S {} 
 # plugins=(git mvn docker colored-man-pages)
 # omz plugin enable git mvn docker colored-man-pages
 
+function wsl2_pageant(){
+  export SSH_AUTH_SOCK="$HOME/.ssh/agent.sock"
+  if ! ss -a | grep -q "$SSH_AUTH_SOCK"; then
+    rm -f "$SSH_AUTH_SOCK"
+    wsl2_ssh_pageant_bin="$HOME/.ssh/wsl2-ssh-pageant.exe"
+    if test -x "$wsl2_ssh_pageant_bin"; then
+      (setsid nohup socat UNIX-LISTEN:"$SSH_AUTH_SOCK,fork" EXEC:"$wsl2_ssh_pageant_bin" >/dev/null 2>&1 &)
+    else
+      echo >&2 "WARNING: $wsl2_ssh_pageant_bin is not executable."
+    fi
+    unset wsl2_ssh_pageant_bin
+  fi
+}
+
+uname -a | grep WSL2 && wsl2_pageant
+
