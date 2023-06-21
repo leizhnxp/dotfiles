c92d63ca35b1e50228207275f84bce3686f4d964
leizh <leizhnxp@gmail.com>
2023-06-21 15:17:18 +0800

    should say bye to GNU screen

--- files/my-oh-zsh.zsh
+++ files/my-oh-zsh.zsh
@@ -1,4 +1,4 @@
-echo hello omz!
+echo hello omz! \[AT\] `date` \[ON\] `uname -a`
 
 # follow the first line of original zshrc?
 export PATH=$HOME/bin:/usr/local/bin:$PATH
@@ -13,9 +13,11 @@ unset LESS
 # for ... amazing in everyday ... deprecated, to config by omz theme
 # ZSH_THEME="random"
 
+## DEPRECATED screen package is removed in RHEL 8
+## https://access.redhat.com/solutions/4136481
 # for ssh agent forwarding on next terminal login to attach screen session
 # but to skip on Cygwin and OSX
-uname -a | grep Linux && ls /var/run/screen/S-$USER | xargs -i -n1 screen -S {} -X setenv SSH_AUTH_SOCK $SSH_AUTH_SOCK
+# uname -a | grep Linux && ls /var/run/screen/S-$USER | xargs -i -n1 screen -S {} -X setenv SSH_AUTH_SOCK $SSH_AUTH_SOCK
 
 # my favorite tools , deprecated, to config by omz plugin 
 # plugins=(git mvn docker colored-man-pages)
