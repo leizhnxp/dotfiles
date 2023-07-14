01d9bcdc282fcc83d014159415fe0fc00c4f98fa
leizh <leizhnxp@gmail.com>
2023-07-14 14:11:10 +0800

    disable the screen on bash

--- files/my-oh-bash.sh
+++ files/my-oh-bash.sh
@@ -4,5 +4,5 @@ echo hello in bash?
 
 # only in native Linux or WSL
 # Cygwin or OSX skipped
-uname -a | grep Linux && ls /var/run/screen/S-$USER | xargs -i -n1 screen -S {} -X setenv SSH_AUTH_SOCK $SSH_AUTH_SOCK
+# uname -a | grep Linux && ls /var/run/screen/S-$USER | xargs -i -n1 screen -S {} -X setenv SSH_AUTH_SOCK $SSH_AUTH_SOCK
 
