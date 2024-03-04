5d5b0495231c2d50c6333259eabf8b0a1fff13fe
zhenhuea.lei <leizhnxp@gmail.com>
2024-03-04 16:28:10 +0800

    install github copilot for linux dist
    + github cli and waiting auth login
    + github cli copilot extension plugin
    + github for vim

---
+++ provision/gists/github/install-github-copilot.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+## github cli tools copilot plugin
+## https://docs.github.com/zh/copilot/github-copilot-in-the-cli/using-github-copilot-in-the-cli
+
+gh extension install github/gh-copilot
+
+## install vim and vim plugin
+## https://docs.github.com/zh/copilot/using-github-copilot/getting-started-with-github-copilot?tool=vimneovim#prerequisites
+
+conda install conda-forge::vim conda-forge::nodejs
+
