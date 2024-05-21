bc326f920e5184d381e760b3879b3fbfde87ecc1
zhenhua.lei <leizhnxp@gmail.com>
2024-05-21 17:36:05 +0800

    script as docker ce for debian

---
+++ provision/gists/docker/engine/debian/uninstall.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# ref https://docs.docker.com/engine/install/debian/#uninstall-docker-engine
+
+sudo apt-get purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+
+
