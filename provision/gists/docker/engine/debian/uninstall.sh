23cd94dc1fdb5aa9f3830812df5a1bfd3f23887e
leizh <leizhnxp@users.noreply.github.com>
2024-05-24 18:22:06 +0800

    add scripts as installing docker-ce
    + on debian-like os

--- provision/gists/docker/engine/debian/uninstall.sh
+++ provision/gists/docker/engine/debian/uninstall.sh
@@ -2,6 +2,9 @@
 
 # ref https://docs.docker.com/engine/install/debian/#uninstall-docker-engine
 
-sudo apt-get purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+sudo apt-get purge -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
 
+sudo apt-get autoremove -y
+
+sudo rm -f /etc/apt/sources.list.d/docker.list
 
